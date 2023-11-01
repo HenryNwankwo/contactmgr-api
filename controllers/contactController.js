ac99dc43a11ceab446498e99ce2c8d871e7cfaf0
Nwankwo Henry <nameisifunanya@gmail.com>
2023-11-01 15:43:46 +0100

    Used async handler funtion for all route controller, added another error status code to error handler middleware

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,47 +1,49 @@
+const asyncHandler = require('express-async-handler');
+
 /* Get all contacts
 @route GET /api/contacts
 @access public */
 
-const getAllContacts = (req, res) => {
+const getAllContacts = asyncHandler(async (req, res) => {
   res.status(200).send('Getting all contacts');
-};
+});
 
 /* Get contact by ID
 @route GET /api/contacts/:id
 @access public */
 
-const getContactById = (req, res) => {
+const getContactById = asyncHandler(async (req, res) => {
   res.status(200).send(`Getting contact for ${req.params.id}`);
-};
+});
 
 /* create new contact
 @route POST /api/contacts
 @access public */
 
-const createContact = (req, res) => {
+const createContact = asyncHandler(async (req, res) => {
   const { name, phone, email } = req.body;
 
   if (!name || !phone || !email) {
     res.status(400);
     throw new Error('All fields are required! ');
   }
-};
+});
 
 /* Update contact
 @route PUT /api/contacts/:id
 @access public */
 
-const updateContact = (req, res) => {
+const updateContact = asyncHandler(async (req, res) => {
   res.status(200).send(`Updating contact for ${req.params.id}`);
-};
+});
 
 /* Delete contact
 @route DELETE /api/contacts/:id
 @access public */
 
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler(async (req, res) => {
   res.send(`Delete contact for ${req.params.id}`);
-};
+});
 
 module.exports = {
   getAllContacts,
--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,10 +1,11 @@
+const { constants } = require('../constants');
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode ? res.statusCode : 500;
 
   //Displaying error depending on error type and status code
 
   switch (statusCode) {
-    case 400:
+    case constants.VALIDATION_ERROR:
       res.json({
         title: 'Validation Failed!',
         message: err.message,
@@ -12,15 +13,38 @@ const errorHandler = (err, req, res, next) => {
       });
       break;
 
-    case 404:
+    case constants.UNAUTHORIZED:
+      res.json({
+        title: 'Unauthorized',
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+
+    case constants.FORBIDDEN:
+      res.json({
+        title: 'Forbidden',
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+
+    case constants.NOT_FOUND:
       res.json({
         title: 'Not Found',
         message: err.message,
         stackTrace: err.stack,
       });
       break;
-
+    case constants.SERVER_ERROR:
+      res.json({
+        title: 'Server Error',
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
     default:
+      console.log('Error: ', err.message);
       break;
   }
 };
