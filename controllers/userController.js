d881d3ccf4f77798122f31ead2dea7f3807669f1
Nwankwo Henry <nameisifunanya@gmail.com>
2023-11-03 14:25:39 +0100

    Implemented the user registration

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler');
 const User = require('./../models/userModel');
+const bcrypt = require('bcrypt');
 
 /* =================================
 @desc Register a user
@@ -8,7 +9,48 @@ const User = require('./../models/userModel');
 ==================================*/
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(201).json({ message: 'User registration successful!' });
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error('All fields is required!');
+  }
+  //Checking if username or email is taken
+
+  const usernameExist = await User.findOne({ username });
+  const emailExist = await User.findOne({ email });
+
+  if (usernameExist) {
+    res.status(400);
+    throw new Error('Username already exists!');
+  }
+  if (emailExist) {
+    res.status(400);
+    throw new Error('Email already exists!');
+  }
+
+  //Hashing the password
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  //Creating new user
+  const newUser = await User.create({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
+  if (newUser) {
+    res.status(201).json({
+      message: 'User registration successful!',
+      user: {
+        _id: newUser.id,
+        username: newUser.username,
+        email: newUser.email,
+      },
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data, user registration failed!');
+  }
 });
 
 /* =================================
