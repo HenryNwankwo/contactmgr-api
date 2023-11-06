1deb49cbbbe611b4b7b03a731fba4805ab0ac9e3
Nwankwo Henry <nameisifunanya@gmail.com>
2023-11-06 15:55:41 +0100

    Added the getcontact by id docs

--- models/contactModel.js
+++ models/contactModel.js
@@ -101,6 +101,46 @@ const mongoose = require('mongoose');
  *              type: number
  *              format: int64
  *              example: 0
+ *
+ *    GetContactById:
+ *      type: object
+ *      required:
+ *        - id
+ *        - user_id
+ *        - name
+ *        - email
+ *        - phone
+ *        - createdAt
+ *        - updatedAt
+ *        - __v
+ *      properties:
+ *            id:
+ *              type: string
+ *              example: 65451bae2bed43bfc1a3ca85
+ *            user_id:
+ *              type: string
+ *              example: 6544f0b429c651473da29777
+ *            name:
+ *              type: string
+ *              example: Henry Ubanese
+ *            email:
+ *              type: string
+ *              example: Chinwuba@hotmail.com
+ *            phone:
+ *              type: string
+ *              example: +2347080000000
+ *            createdAt:
+ *              type: string
+ *              format: date-time
+ *              example: 2023-11-03T16:11:26.819Z
+ *            updatedAt:
+ *              type: string
+ *              format: date-time
+ *              example: 2023-11-03T16:36:59.407Z
+ *            __v:
+ *              type: number
+ *              format: int64
+ *              example: 0
  */
 
 const contactSchema = mongoose.Schema(
--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -73,7 +73,42 @@ router.use(validateToken);
 
 router.route('/').get(getAllContacts).post(createContact);
 
-//For getting a contact
+/**=========================
+ * For getting a contact by id
+ * =======================**/
+
+/**
+ * @openapi
+ * /api/v1/contacts/{id}:
+ *  get:
+ *    summary: returns an object containing a contact details
+ *    tags:
+ *      - Contacts
+ *    parameters:
+ *      - name: id
+ *        in: path
+ *        description: id of the contact
+ *        required: true
+ *    responses:
+ *      200:
+ *        description: Successful GET request.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/GetContactById'
+ *      400:
+ *       description: Validation failed
+ *      401:
+ *       description: Unauthorized
+ *      403:
+ *       description: Forbidden
+ *      404:
+ *       description: Contact not found
+ *      500:
+ *       description: Server Error
+ *
+ */
+
 router.route('/:id').get(getContactById);
 
 //For updating contact
