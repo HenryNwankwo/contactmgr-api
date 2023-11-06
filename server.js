b61df86c78955f8055650f2bb3f9c82b6d50c402
Nwankwo Henry <nameisifunanya@gmail.com>
2023-11-06 11:43:13 +0100

    Created the basic setup for swagger ui

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const errorHandler = require('./middlewares/errorHandler');
 const dbConnection = require('./config/dbConnect');
 const contactRoutes = require('./routes/contactRoutes');
 const userRoutes = require('./routes/userRoutes');
+const swaggerDocs = require('./config/swagger');
 
 const app = express();
 const port = process.env.PORT || 5001;
@@ -19,4 +20,6 @@ app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server running at port: ${port}`);
+
+  swaggerDocs(app, port);
 });
