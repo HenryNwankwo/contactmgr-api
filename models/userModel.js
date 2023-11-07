5e8551989bc3674fad289ca8583ebee8d53d798e
Nwankwo Henry <nameisifunanya@gmail.com>
2023-11-07 09:41:45 +0100

    Added the user registration documentation

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,51 @@
 const mongoose = require('mongoose');
 
+/**=========================
+ * User Schemas
+ * =======================**/
+
+/**
+ * @openapi
+ * components:
+ *  schemas:
+ *    RegisterUser:
+ *      type: object
+ *      required:
+ *        - username
+ *        - email
+ *        - password
+ *      properties:
+ *        username:
+ *          type: string
+ *          example: janiceOkoro24
+ *        email:
+ *          type: string
+ *          example: okoroJanice@gmail.com
+ *        password:
+ *          type: string
+ *          format: password
+ *          example: mypassword1234
+ *
+ *    RegisterUserResponse:
+ *      type: object
+ *      properties:
+ *        message:
+ *          type: string
+ *          example: User registration successful!
+ *        user:
+ *          type: object
+ *          properties:
+ *            _id:
+ *              type: string
+ *              example: 6549f68f42d3faebf3c01ec3
+ *            username:
+ *              type: string
+ *              example: janiceOkoro24
+ *            email:
+ *              type: string
+ *              example: okoroJanice@gmail.com
+ */
+
 const userSchema = mongoose.Schema(
   {
     username: {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,7 +6,37 @@ const {
   getCurrentUser,
 } = require('./../controllers/userController');
 
-//For registering a user
+/**=========================
+ * For Registering a User
+ * =======================**/
+
+/**
+ * @openapi
+ * /api/vi/users/register:
+ *  post:
+ *    summary: For user registration. Returns the object of the user created
+ *    tags:
+ *      - Users
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#components/schemas/RegisterUser'
+ *    responses:
+ *      201:
+ *        description: Successful user creation.
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#components/schemas/RegisterUserResponse'
+ *      400:
+ *       description: Bad Request
+ *      409:
+ *       description: Request Conflict
+ *      500:
+ *       description: Server Error
+ */
 
 router.post('/register', registerUser);
 
